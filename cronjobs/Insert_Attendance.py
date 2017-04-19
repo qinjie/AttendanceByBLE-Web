53b8978b2d13b321b7adb6e14471f72fbb8b1b08
HoaNP <hoanpse03457@fpt.edu.vn>
2017-04-19 16:50:14 +0800

    update crontab

--- cronjobs/Insert_Attendance.py
+++ cronjobs/Insert_Attendance.py
@@ -30,12 +30,18 @@ def insert_attendance(cursor, student_id, lesson_date_id, recorded_time, status)
   #  print(sql)
     try:
         cursor.execute(sql)
-     #   print(10)
+        # print(10)
     except Exception :
         error = "This student was take attendance"
 
 def get_attendance(cursor) :
     sql = "SELECT student_id, lesson_date_id FROM attendance"
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+def get_all_semmester(cursor):
+    sql = "SELECT start_date, end_date " \
+          "FROM semester_info"
     cursor.execute(sql)
     return cursor.fetchall()
 
@@ -46,25 +52,33 @@ if __name__ == '__main__' :
                                  password=_password,
                                  database=_database)
     cursor = connection.cursor()
-
+    list_semester = get_all_semmester(cursor)
+    print(list_semester)
     yesterday = datetime.datetime.now().date() - datetime.timedelta(days = 1)
-    list_lesson_id = get_lesson_id_at_date(cursor, yesterday)
-    tmp = get_attendance(cursor)
-    list_attendance = []
-    for a in tmp :
-        t = str(a[0]) + "a" + str(a[1])
-        list_attendance.append(t)
+    for a in list_semester :
+        start_date = a[0]
+        end_date = a[1]
 
-    for a in list_lesson_id :
-        lesson_id = a[0]
-        lesson_date_id = a[1]
-        #print(lesson_id)
-        list_timetable = get_time_table(cursor, lesson_id)
-        for b in list_timetable :
-            student_id = b[0]
-            recorded_time = datetime.datetime.now().time()
-            status = -1
-            insert_attendance(cursor, student_id, lesson_date_id, recorded_time, status)
+        if (start_date < yesterday) & (yesterday < end_date) :
+            while(start_date < end_date) :
+                list_lesson_id = get_lesson_id_at_date(cursor, start_date)
+                start_date  = start_date + datetime.timedelta(days=1)
+                tmp = get_attendance(cursor)
+                list_attendance = []
+                for a in tmp :
+                    t = str(a[0]) + "a" + str(a[1])
+                    list_attendance.append(t)
+
+                for a in list_lesson_id :
+                    lesson_id = a[0]
+                    lesson_date_id = a[1]
+                    #print(lesson_id)
+                    list_timetable = get_time_table(cursor, lesson_id)
+                    for b in list_timetable :
+                        student_id = b[0]
+                        recorded_time = datetime.datetime.now().time()
+                        status = -1
+                        insert_attendance(cursor, student_id, lesson_date_id, recorded_time, status)
 
 
     cursor.close()
